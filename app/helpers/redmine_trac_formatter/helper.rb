a951948d47d08b6afe9245e65156dbc0325ad2a2
Jeremy Thomerson <jeremy@thomersonfamily.com>
2014-06-27 11:00:09 -0400

    remove the wikitoolbar_for
    There was an error on newer versions of Ruby and this
    toolbar has never really been great anyway, so I just
    removed it. If anyone wants to add a helpful toolbar
    back in, feel free to send a pull request.

--- app/helpers/redmine_trac_formatter/helper.rb
+++ app/helpers/redmine_trac_formatter/helper.rb
@@ -3,56 +3,7 @@ module RedmineTracFormatter
     unloadable
 
     def wikitoolbar_for(field_id)
-      file = 'http://trac.edgewall.org/wiki/WikiFormatting#Cheatsheet'
-      help_link = l(:setting_text_formatting) + ': ' +
-      link_to(l(:label_help), file,
-              :onclick => "window.open(\"#{file}\", \"\", \"resizable=yes, location=no, width=300, height=640, menubar=no, status=no, scrollbars=yes\"); return false;")
-
-      heads_for_wiki_formatter
-
-      javascript_include_tag('jstoolbar/jstoolbar') +
-        javascript_include_tag('trac', :plugin => 'redmine_trac_formatter') +
-        javascript_include_tag("lang/trac-#{current_language}", :plugin => 'redmine_trac_formatter') +
-        javascript_tag(<<-EOS + (Setting.plugin_redmine_trac_formatter[:trac_formatter_require_block].to_s == 'true' ? <<-EOT : ''))
-          var editor = $('#{field_id}');
-          var toolbar = new jsToolBar($('#{field_id}'));
-          toolbar.setHelpLink('#{help_link}');
-          toolbar.draw();
-        EOS
-          var toggler = document.createElement('div');
-          toggler.className = 'jsToggler';
-          var toggleButton = document.createElement('button');
-          toggleButton.setAttribute('type', 'button');
-          toggleButton.className = 'jsToggleButton jst_disabled';
-          toggleButton.title = 'Trac';
-          toggleButton.innerHTML = '<s>Trac</s>';
-          toggler.appendChild(toggleButton);
-
-          var toolbarElement = $A(editor.parentNode.parentNode.childNodes).find(function(x){ return x.className == 'jstElements'});
-          toolbarElement.parentNode.insertBefore(toggler, toolbarElement);
-          Element.hide(toolbarElement);
-
-          handler = function(){
-            Element.toggle(toolbarElement);
-
-            var src = editor.value
-            if (toggleButton.className.split(' ').include('jst_disabled')) {
-              toggleButton.className = 'jsToggleButton jst_enabled';
-              toggleButton.innerHTML = '<b>Trac</b>';
-              if (!/^\\s*=begin/.test(src)) {
-                src = "=begin\\n" + src;
-                if (src[src.length - 1] != "\\n") src += "\\n";
-                src += "=end\\n";
-                editor.value = src;
-              }
-            } else {
-              toggleButton.className = 'jsToggleButton jst_disabled';
-              toggleButton.innerHTML = '<s>Trac</s>';
-              editor.value = src.replace(/^\\s*=begin\\n/, '').replace(/=end\\n\\s*$/, '');
-            }
-          };
-          Event.observe(toggleButton, 'click', handler);
-        EOT
+        heads_for_wiki_formatter
     end
 
     def initial_page_content(page)
@@ -60,13 +11,11 @@ module RedmineTracFormatter
     end
 
     def heads_for_wiki_formatter
-      unless @heads_for_wiki_formatter_included
-        content_for :header_tags do
-          stylesheet_link_tag('jstoolbar.css') +
-            stylesheet_link_tag('trac.css', :plugin => 'redmine_trac_formatter')
-        end
-        @heads_for_wiki_formatter_included = true
-      end
+          unless @heads_for_wiki_formatter_included
+            content_for :header_tags do
+            end
+            @heads_for_wiki_formatter_included = true
+          end
     end
   end
 end
